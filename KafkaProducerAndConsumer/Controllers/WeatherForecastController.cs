04182817eaa3f40eff31bdf10169f0999319b49b
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-27 23:26:31 +0600

    change method Get to publish weatherForecasts

--- KafkaProducerAndConsumer/Controllers/WeatherForecastController.cs
+++ KafkaProducerAndConsumer/Controllers/WeatherForecastController.cs
@@ -23,12 +23,19 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+        foreach (var weatherForecast in weatherForecasts)
+        {
+            _producer.Publish(weatherForecast);
+        }
+        
+        return weatherForecasts;
     }
 }
