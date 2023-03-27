6e81cb2ffa0cfa6c14f218f75a30e80d9d851f76
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-27 23:47:38 +0600

    inject IProducer with int key and message type weatherForecast into a wrapper WeatherProducer

--- KafkaProducerAndConsumer/WeatherProducer.cs
+++ KafkaProducerAndConsumer/WeatherProducer.cs
@@ -1,7 +1,15 @@
-﻿namespace KafkaProducerAndConsumer;
+﻿using Confluent.Kafka;
+
+namespace KafkaProducerAndConsumer;
 
 public class WeatherProducer : IWeatherProducer
 {
+    private readonly IProducer<int, WeatherForecast> _producer;
+
+    public WeatherProducer(IProducer<int, WeatherForecast> producer)
+    {
+        _producer = producer;
+    }
     public void Publish(WeatherForecast weatherForecast)
     {
         throw new NotImplementedException();
